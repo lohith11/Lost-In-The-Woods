564bf4fdcf133e46e9d48ebb6a377e51ef74085a
Vinay Datta <vinaydatta50@gmail.com>
2023-04-15 23:50:07 +0530

    Camera tilt Fixed

--- Assets/Scripts/PlayerScripts/MouseLook.cs
+++ Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -18,6 +18,7 @@ public class MouseLook : MonoBehaviour
     private void Update()
     {
         Rotation();
+
     }
 
     public void Rotation()
@@ -26,19 +27,10 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -20f, 20f);
+        xRotation = Mathf.Clamp(xRotation, -30f, 30f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerObject.Rotate(Vector3.up * playerStateMachine.playerRotation.x + Vector3.up * mouseX);
+        Vector3 rot = Vector3.up * mouseX;
+        playerObject.Rotate(rot);
     }
-
-    //public void CameraHeight()
-    //{
-    //    if(Mathf.Abs(transform.localPosition.y - desiredHeight) > 0.05f)
-    //    {
-    //        //transform.localPosition = Mathf.Lerp(currentPosition, playerStateMachine.standingHeight, 0.1f);
-    //        playerStateMachine.playerCamera.localPosition = new Vector3(0, Mathf.Lerp(transform.localPosition.y, desiredHeight, 0.1f), 0.2f);
-    //        //playerStateMachine.originalPosition = currentPosition;
-    //    }
-    //}
 }
