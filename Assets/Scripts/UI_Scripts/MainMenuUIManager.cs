a6add7c167016d6412f6fd9b31855462bf55bb20
Vinay Datta <vinaydatta50@gmail.com>
2023-04-29 17:49:27 +0530

    Brightness

--- Assets/Scripts/UI_Scripts/MainMenuUIManager.cs
+++ Assets/Scripts/UI_Scripts/MainMenuUIManager.cs
@@ -1,8 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class MainMenuUIManager : MonoBehaviour
 {
-    public GameObject option;
+    public GameObject option1;
+    public GameObject option2;
+    public GameObject option3;
+    public GameObject option4;
+    public GameObject option5;
+    public GameObject option6;
+
+    public VolumeProfile volumeProfile;
+    private ColorAdjustments colorAdjustments;
+
+    public TMP_Text sliderValue;
+    public Slider brightnessSlider;
+
+    private void Start()
+    {
+        volumeProfile.TryGet(out colorAdjustments);
+    }
+
+    private void Update()
+    {
+        sliderValue.text = " " + brightnessSlider.value;
+        SetBrightness(brightnessSlider.value);
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        colorAdjustments.postExposure.value = brightness;
+    }
+
 }
