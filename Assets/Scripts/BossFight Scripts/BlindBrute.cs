9d67806e03b2471758ee272ce264e588f7dcda9b
Lohith sai <45137887+lohith11@users.noreply.github.com>
2023-05-05 23:28:25 +0530

    Added new attack patterns for the boss

--- Assets/Scripts/BossFight Scripts/BlindBrute.cs
+++ Assets/Scripts/BossFight Scripts/BlindBrute.cs
@@ -9,7 +9,7 @@ public class BlindBrute : MonoBehaviour
 
     [SerializeField] float maxHealth;
     public float health;
-    [SerializeField] float damage;
+    [SerializeField] float slashDamage;
     [SerializeField] float range;
     public NavMeshAgent agent;
     public Animator bossAnimator;
@@ -20,13 +20,28 @@ public class BlindBrute : MonoBehaviour
     public LayerMask targetMask, obstructionMask;
     public float angle;
     public float attackRadius;
+    public GameObject player;
+    public PlayerHealth playerHealth;
+    public GameObject attackPoint;
+
+    [Header("Parabolic jump")]
+    [Space(5f)]
+    //[SerializeField] Transform targetPosition;
+    public float pounceHeight = 15f;
+    public float pounceDuration = 3.0f;
+    public Transform playerTpPoint;
+    [Space(2f)]
+
+
 
 
     public PlayerStateMachine playerStateMachine;
+    public static EventHandler<dealDamageEventArg> bossDamage;
 
     void Start()
     {
-        playerStateMachine = FindObjectOfType<PlayerStateMachine>();
+        player = GameObject.Find("Playerrr");
+        playerStateMachine = player.GetComponent<PlayerStateMachine>();
         Barrel.explosiveDamage += TakeDamage;
         GetComponent<NavMeshAgent>();
         GetComponent<Animator>();
@@ -39,21 +54,28 @@ public class BlindBrute : MonoBehaviour
         Debug.Log("The boss health is : " + health);
         if (Input.GetKeyDown(KeyCode.G))
         {
-            AOEAttack();
+            //AOEAttack();
+            MoveAttack();
+            // PounceAttack();
         }
-        if(PlayerInRange)
-            transform.LookAt(playerStateMachine.transform.position);
+
+        agent.stoppingDistance = stoppingDistance;
+        // if(PlayerInRange)
+        //     transform.LookAt(playerStateMachine.transform.position);
     }
 
     private void FixedUpdate()
     {
         float distanceToPlayer = Vector3.Distance(transform.position, playerStateMachine.transform.position);
-        if(PlayerInRange && distanceToPlayer< attackRadius)
+        if (PlayerInRange && distanceToPlayer < attackRadius)
         {
             SlashAttack();
         }
-        if(distanceToPlayer > attackRadius)
+        if (distanceToPlayer > attackRadius && PlayerInRange)
+        {
             bossAnimator.Play("Idle Anim");
+            ChasePlayer();
+        }
     }
 
     public void DefaultValues()
@@ -66,14 +88,34 @@ public class BlindBrute : MonoBehaviour
         health -= e.damage;
     }
 
-    public void DealDamage()
+    public void DealDamage(float damage)
     {
-        Debug.Log("The boss dealt damage");
+        Collider[] hitColliders = Physics.OverlapSphere(attackPoint.transform.position, attackRadius, targetMask);
+        if (hitColliders.Length > 0)
+        {
+            if (bossDamage != null)
+            {
+                // bossDamage.Invoke(this, new dealDamageEventArg { damage = damage });
+                Debug.Log("The boss dealt damage");
+                PlayerHealth playerHealth = hitColliders[0].GetComponent<PlayerHealth>();
+                if (playerHealth != null)
+                {
+                    playerHealth.TakeDamage(damage);
+                }
+            }
+            if (playerHealth == null)
+            {
+                Debug.Log("player not found");
+            }
+        }
+
     }
 
     private void SlashAttack()
     {
         bossAnimator.Play("Normal_Attack");
+        DealDamage(slashDamage);
+
     }
 
     public void AOEAttack()
@@ -104,15 +146,40 @@ public class BlindBrute : MonoBehaviour
     {
         Debug.Log("Move attack called");
         int targetPosition = UnityEngine.Random.Range(0, teleportPoints.Length);
-        Debug.Log("Target position is : " + targetPosition);
         agent.SetDestination(teleportPoints[targetPosition].position);
         bossAnimator.Play("Walk");
-        agent.speed = 25f;
-        yield return new WaitForSeconds(15f);
-        agent.stoppingDistance = stoppingDistance;
-        agent.SetDestination(playerStateMachine.transform.position);
+        yield return new WaitForSeconds(2f);
+        PounceAttack();
     }
 
+    private void ChasePlayer()
+    {
+
+    }
+
+    private void PounceAttack()
+    {
+        Vector3 startPosition = transform.position;
+        float startTime = Time.time;
+        StartCoroutine(PounceCoroutine(startPosition, startTime, playerTpPoint.transform));
+    }
+
+    IEnumerator PounceCoroutine(Vector3 startPosition, float startTime, Transform targetPosition)
+    {
+        float timeElapsed = 0.0f;
+
+        while (timeElapsed < pounceDuration)
+        {
+            float normalizedTime = timeElapsed / pounceDuration;
+            float parabolicTime = 1 - 4 * (normalizedTime - 0.5f) * (normalizedTime - 0.5f);
+            transform.position = Vector3.Lerp(startPosition, targetPosition.position, normalizedTime) + Vector3.up * parabolicTime * pounceHeight;
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition.position;
+    }
 
     private void FieldOfViewCheck()
     {
