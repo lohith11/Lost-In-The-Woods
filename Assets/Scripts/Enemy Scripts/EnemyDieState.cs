56e3cd584290dc1fc64cdf20dedc0006ee9f75f8
Vinay Datta <vinaydatta50@gmail.com>
2023-05-09 03:41:18 +0530

    UngaBunga deathAnim

--- Assets/Scripts/Enemy Scripts/EnemyDieState.cs
+++ Assets/Scripts/Enemy Scripts/EnemyDieState.cs
@@ -7,10 +7,7 @@ public class EnemyDieState : EnemyBaseState
     public override void EnterState()
     {
         Debug.Log("Die state");
-        AlertEnemies();
-        enemyStateManager.enemyAnimController.CrossFade("Death_Anim", 0.2f);
-        enemyStateManager.enemyHealth.health = 0;
-
+        enemyStateManager.enemyAnimController.Play("Death_Anim");
     }
     public override void UpdateState()
     {
@@ -21,19 +18,4 @@ public class EnemyDieState : EnemyBaseState
     {
 
     }
-
-
-    private void AlertEnemies()
-    {
-        Collider[] nearEnemies = Physics.OverlapSphere(enemyStateManager.enemyAgent.transform.position, enemyStateManager.detectRange);
-        foreach (Collider enemies in nearEnemies)
-        {
-            var enemyManager = enemies.GetComponent<EnemyStateManager>();
-            if (nearEnemies.Length > 0)
-            {
-                enemyManager.switchState(enemyManager.AlertState);
-            }
-        }
-    }
-
 }
