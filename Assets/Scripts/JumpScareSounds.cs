73b43b319105eae696ab2eb7452864093f9f91f2
Vinay Datta <vinaydatta50@gmail.com>
2023-05-09 01:27:36 +0530

    Chapter 2 JumpScares sounds

---
+++ Assets/Scripts/JumpScareSounds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScareSounds : MonoBehaviour
+{
+    private AudioSource jumpScareAudios;
+    private void Start()
+    {
+        jumpScareAudios = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            jumpScareAudios.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            jumpScareAudios = null;
+        }
+    }
+}
